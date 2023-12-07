6fb626846c79e9a6cffc877939b867cbcbe945d1
Yanis Adekalom <yanis971.60@gmail.com>
2023-12-07 14:49:04 +0100

    Feat : Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Objet pouvant récupérer une saisie dans la console
+        //String line = scanner.nextLine();
+        //int saisie = scanner.nextInt();
+        /*boolean invalid = true;
+        do {
+            try {
+                System.out.println("Saisissez un entier : ");
+                Scanner scanner = new Scanner(System.in);
+                int saisie = scanner.nextInt();
+                invalid = false;
+                System.out.println(saisie);
+            } catch (Exception e) {
+                System.out.println("Saisie invalide.");
+            }
+        } while (invalid);*/
+        System.out.println("Saisissez un entier : ");
+        System.out.println(getInputInt());
+    }
+
+    private static int getInputInt() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextInt();
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Saisie invalide.");
+            return getInputInt();
+        }
     }
 }
