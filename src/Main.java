9c04342d32ea262d695bf90c44e0ed69188eca81
Yanis Adekalom <yanis971.60@gmail.com>
2023-12-07 16:34:06 +0100

    Fix : Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import com.sun.security.jgss.GSSUtil;
+import flux.Streams;
+import flux.lambda.Lambdas;
+import objects.Jardin;
+import operations.Boucles;
+import operations.Conditions;
+import operations.MathsOperations;
+import types.Types;
+
+import java.util.*;
+import java.util.function.Consumer;
 
 public class Main {
+    private static final Map<String, Consumer<String[]>> MAINS = Map.of(
+            "TYPES", Types::main,
+            "MATHS", MathsOperations::main,
+            "CONDITIONS", Conditions::main,
+            "BOUCLES", Boucles::main,
+            "CLASSES", Jardin::main,
+            "LAMBDAS", Lambdas::main,
+            "STREAMS", Streams::main
+    );
+
+    private static final List<String> MAINS_KEYS = MAINS.keySet().stream().toList();
+    // Un Set<> est une List<> sans doublons
+    //private static final Set<String> MAINS_KEYS = MAINS.keySet();
+
     public static void main(String[] args) {
 
         // Objet pouvant récupérer une saisie dans la console
@@ -19,8 +42,9 @@ public class Main {
                 System.out.println("Saisie invalide.");
             }
         } while (invalid);*/
-        System.out.println("Saisissez un entier : ");
-        System.out.println(getInputInt());
+      /*  System.out.println("Saisissez un entier : ");
+        System.out.println(getInputInt());*/
+        displayMenu();
     }
 
     private static int getInputInt() {
@@ -30,6 +54,13 @@ public class Main {
         } catch (InputMismatchException inputMismatchException) {
             System.out.println("Saisie invalide.");
             return getInputInt();
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("Saisissez le numéro du programme à lancer :");
+        for (String key : MAINS_KEYS) {
+            System.out.printf("%d\t->\t%s%n", MAINS_KEYS.indexOf(key), key);
         }
     }
 }
