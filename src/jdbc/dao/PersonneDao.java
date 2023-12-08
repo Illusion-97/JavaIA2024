d06beccd0a1f0510b96174a5a4b0ed58eccaf7d0
Yanis Adekalom <yanis971.60@gmail.com>
2023-12-08 15:13:01 +0100

    Feat : Create Table

--- src/jdbc/dao/PersonneDao.java
+++ src/jdbc/dao/PersonneDao.java
@@ -12,7 +12,8 @@ public class PersonneDao implements IPersonneDao {
     public void createTable() {
         // Statement représente une requête envoyée au SGBD
         try (Statement statement = DbConnection.getInstance().createStatement()) {
-
+            String sql = "CREATE TABLE personne(id INT(10) NOT NULL AUTO_INCREMENT, version INT(10), nom VARCHAR(20), prenom VARCHAR(20), role VARCHAR(15) , PRIMARY KEY ( id )) ";
+            statement.execute(sql);
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
