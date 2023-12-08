e3dd4aa1e256f89da8cc5ac503e37256d0b10167
Yanis Adekalom <yanis971.60@gmail.com>
2023-12-08 15:05:46 +0100

    Feat : Statement

--- src/jdbc/dao/PersonneDao.java
+++ src/jdbc/dao/PersonneDao.java
@@ -1,13 +1,21 @@
 package jdbc.dao;
 
+import jdbc.DbConnection;
 import jdbc.models.Personne;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class PersonneDao implements IPersonneDao {
     @Override
     public void createTable() {
+        // Statement représente une requête envoyée au SGBD
+        try (Statement statement = DbConnection.getInstance().createStatement()) {
 
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
