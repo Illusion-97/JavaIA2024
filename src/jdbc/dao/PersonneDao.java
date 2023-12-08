be2ff8099abd2a645de5c5cbe446fc01e2493cd3
Yanis Adekalom <yanis971.60@gmail.com>
2023-12-08 15:18:10 +0100

    Fix : Create Table

--- src/jdbc/dao/PersonneDao.java
+++ src/jdbc/dao/PersonneDao.java
@@ -12,7 +12,7 @@ public class PersonneDao implements IPersonneDao {
     public void createTable() {
         // Statement représente une requête envoyée au SGBD
         try (Statement statement = DbConnection.getInstance().createStatement()) {
-            String sql = "CREATE TABLE personne(id INT(10) NOT NULL AUTO_INCREMENT, version INT(10), nom VARCHAR(20), prenom VARCHAR(20), role VARCHAR(15) , PRIMARY KEY ( id )) ";
+            String sql = "CREATE TABLE IF NOT EXISTS personne(id INT(10) NOT NULL AUTO_INCREMENT, version INT(10), nom VARCHAR(20), prenom VARCHAR(20), role VARCHAR(15), PRIMARY KEY ( id )) ";
             statement.execute(sql);
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
@@ -42,5 +42,10 @@ public class PersonneDao implements IPersonneDao {
     @Override
     public boolean delete(long id) {
         return false;
+    }
+
+    public static void main(String[] args) {
+        IPersonneDao personneDao = new PersonneDao();
+        personneDao.createTable();
     }
 }
