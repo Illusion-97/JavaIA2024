06e1a031407b721da78ed55065bb63f6468fe943
Yanis Adekalom <yanis971.60@gmail.com>
2023-12-08 09:34:11 +0100

    Feat : Annuaire

---
+++ src/serialization/Annuaire.java
@@ -0,0 +1,67 @@
+package serialization;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Annuaire implements Serializable {
+    private String nom;
+    private Person owner;
+    private List<Person> contacts;
+
+    public Annuaire() {
+    }
+
+    public Annuaire(String nom, Person owner, List<Person> contacts) {
+        this.nom = nom;
+        this.owner = owner;
+        this.contacts = contacts;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public List<Person> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Person> contacts) {
+        this.contacts = contacts;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nom : %s%nOwner : %s%nContacts : {%n%s%n}",
+                nom,
+                // utilisera automatiquement sa méthode toString
+                owner,
+                contacts.stream()
+                        .map(Objects::toString)
+                        .collect(Collectors.joining("\n\t")));
+    }
+
+    public static Annuaire createAnnuaire() {
+        List<Person> contacts = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++){
+            contacts.add(new Person("Person " + i, random.nextLong(0, Long.MAX_VALUE)));
+        }
+        return new Annuaire("Mon annuaire", new Person("Yanis", 549685), contacts);
+    }
+}
