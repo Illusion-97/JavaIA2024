67ddec46e71236728f3fa858cdddfbaed34c926c
Yanis Adekalom <yanis971.60@gmail.com>
2023-12-08 09:23:45 +0100

    Feat : Person

---
+++ src/serialization/Person.java
@@ -0,0 +1,45 @@
+package serialization;
+
+import java.io.Serializable;
+
+// Toute classe java est un POJO (Plain Old Java Object)
+//JavaBean est un pojo avec une structure particulière
+/*
+* les attributs sont en visibilité private
+* Le constructeur vide DOIT être présent si un autre l'est également, sinon le constructeur par défaut suffit
+* Les accesseurs (Getter/Setter) doivent être présents pour les attributs
+* Il est impératif d'implémenter l'interface Serializable
+* */
+public class Person implements Serializable {
+    private String nom;
+    private long tel;
+
+    public Person(String nom, long tel) {
+        this.nom = nom;
+        this.tel = tel;
+    }
+
+    public Person() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public long getTel() {
+        return tel;
+    }
+
+    public void setTel(long tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s : %10d", nom, tel);
+    }
+}
